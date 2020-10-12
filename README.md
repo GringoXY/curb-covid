86e48de4a2165c55f9e43169aecabf97ff4f8b0d
GringoXY <przemyslawkowalski01pk@gmail.com>
2020-10-12 20:33:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# app-name
+# Prerequisites
+- Node.js (^10.12.0, or >=12.0.0),
+- npm (^6.4.1, or >=6.9.0).
+
+# Getting started
+  * In order to clone repository use <code>git clone https://github.com/GringoXY/app-name.git</code>
+  * Or you can download it manually and extract that.
+  1. Use <code>npm i</code> in the cmd/terminal to install crucial packages.
+  2. Write down in cmd/terminal <code>npm start</code> in order to start development server.
+*Your cmd/terminal should point to the project's directory 'app-name'*.
+
+# Available commands
+*npm run build* - compiles all code to the *dist* folder for a production purposes,
+*npm start* - starts a server on *locahost:9000*.
+PORT for *locahost* is specified in *webpack.config.js* at line 12.
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+
+  entry: {
+    index: ['./src/js/index.js', './src/scss/index.scss'],
+    contact: ['./src/js/contact.js', './src/scss/contact.scss'],
+  },
+
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist/'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(scss|sass)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: './src/assets/',
+            },
+          },
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[hash].[ext]',
+          outputPath: 'images',
+          publicPath: 'images',
+          omitFile: true,
+          esModule: false,
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+      chunks: ['index'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/contact.html',
+      filename: './contact.html',
+      chunks: ['contact'],
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
