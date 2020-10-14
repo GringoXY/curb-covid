088e595a55db742f6448254b46c21f430dcb4ffb
GringoXY <przemyslawkowalski01pk@gmail.com>
2020-10-14 13:47:56 +0200

    Added covid-19 spread animation

---
+++ src/js/coronavirus-spread-animation.js
@@ -0,0 +1,26 @@
+// take all covid svg
+const svgs = document.querySelectorAll(".c-bar-right__svg");
+
+// covid-19 spread animation
+const spread = (covidSvgs) => {
+  const numberOfCovidSvgs = parseInt(covidSvgs.length, 10);
+
+  for (let i = 0; i <= numberOfCovidSvgs; ++i) {
+    const RANDOMINDEX = Math.floor(Math.random() * numberOfCovidSvgs) + 0;
+    const RANDOMDURATION = (Math.random() * 0.5) + 0.9;
+    const RANDOMYCORD = Math.floor(Math.random() * 100) - 100;
+    const RANDOMXCORD = Math.floor(Math.random() * 100) - 100;
+    const RANDOMSCALE = (Math.random() * 0.3) + 0.1;
+    const RANDOMOPACITY = RANDOMSCALE;
+
+    // randomized spread animation
+    const { style } = covidSvgs[RANDOMINDEX];
+    style.transitionDuration = `${RANDOMDURATION}s`;
+    style.transitionDelay = `${RANDOMDURATION}s`;
+    style.opacity = `${RANDOMOPACITY}`;
+    style.transform =
+      `translate(${RANDOMXCORD}%, ${RANDOMYCORD}%) scale(${RANDOMSCALE})`;
+  }
+}
+
+spread(svgs);
