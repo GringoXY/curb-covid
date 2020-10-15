fc3981903fa5622223b46ff949fb0822035f5591
GringoXY <przemyslawkowalski01pk@gmail.com>
2020-10-15 17:20:31 +0200

    Added theme memorization

--- src/js/contrast-mode.js
+++ src/js/contrast-mode.js
@@ -1,9 +1,20 @@
+function toggleContrastMode(isContrastOn) {
+  const { body } = document;
+  isContrastOn
+    ? body.classList.add("contrast")
+    : body.classList.remove("contrast");
+}
+
 const btnContrast = document.querySelector(".c-settings__btn--is-contrast-on");
-let isContrastSet = false;
+let isContrastSet = localStorage.getItem("contrast");
+
+toggleContrastMode(isContrastSet);
 
 btnContrast.addEventListener("click", () => {
-  isContrastSet = !isContrastSet;
-  const { body } = document;
-
-  isContrastSet ? body.classList.add("contrast") : body.classList.remove("contrast");
+  // make negation of isContrastSet
+  isContrastSet = (isContrastSet === "true")
+    ? ""
+    : "true";
+  localStorage.setItem("contrast", `${isContrastSet}`);
+  toggleContrastMode(isContrastSet);
 });
