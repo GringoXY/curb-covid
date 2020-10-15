44e4eeeb3f1ec79c349f946536a46f2699385a87
GringoXY <przemyslawkowalski01pk@gmail.com>
2020-10-15 20:09:45 +0200

    Added hamburger & nav list manipulation

---
+++ src/js/hamburger.js
@@ -0,0 +1,17 @@
+const btnHamburger = document.querySelector(".c-nav__btn");
+const spanHamburger = document.querySelector(".c-nav__hamburger");
+const navList = document.querySelector(".c-nav__list");
+const { style } = navList;
+let isClicked = false;
+
+btnHamburger.addEventListener("click", () => {
+  isClicked = !isClicked;
+
+  if (isClicked) {
+    style.transform = "translateX(0)";
+    spanHamburger.classList.add("c-nav__hamburger--is-displayed");
+  } else {
+    style.transform = "translateX(100%)";
+    spanHamburger.classList.remove("c-nav__hamburger--is-displayed");
+  }
+});
