07d7697845a92ca3156662653da6be6eafe5a1fc
GringoXY <przemyslawkowalski01pk@gmail.com>
2020-10-16 18:28:35 +0200

    Add statement

--- src/js/functionality/slider.js
+++ src/js/functionality/slider.js
@@ -1,9 +1,9 @@
 const SLIDETIME = 500; //ms
 
-const backButton = document.querySelector('.c-test__btn--back');
-const forwardButton = document.querySelector('.c-test__btn--next');
+const backButton = document.querySelector(".c-test__btn--back");
+const forwardButton = document.querySelector(".c-test__btn--next");
 // Select all fields and convert node to array for easy handling
-const allFields = [...document.querySelectorAll('.c-test__field')];
+const allFields = [...document.querySelectorAll(".c-test__field")];
 let clickable = true;
 let displayed = null;
 let newDisplayed = null;
@@ -12,7 +12,7 @@ function initSlider() {
   // Set the CSS transition on the fields to the value we specified in SLIDETIME above
   allFields.forEach(field =>
     field.setAttribute(
-      'style',
+      "style",
       `transition: transform ${SLIDETIME}ms ease-in;
        animation-duration: ${SLIDETIME}ms;`,
     ),
@@ -22,23 +22,23 @@ function initSlider() {
 function changeField(forward) {
   if (clickable) {
     clickable = false;
-    displayed = document.querySelector('.c-test__field--is-displayed');
+    displayed = document.querySelector(".c-test__field--is-displayed");
     let newDisplayedFieldIndex = parseInt(0, 10);
     const displayedFieldIndex = allFields.indexOf(displayed);
 
     if (forward) {
       newDisplayed = allFields[(displayedFieldIndex + 1) % allFields.length];
       newDisplayedFieldIndex = parseInt(displayedFieldIndex + 1, 10);
-      displayed.classList.add('c-test__field--is-slided-out-left');
-      newDisplayed.classList.add('c-test__field--is-slided-in-right', 'c-test__field--is-displayed');
+      displayed.classList.add("c-test__field--is-slided-out-left");
+      newDisplayed.classList.add("c-test__field--is-slided-in-right", "c-test__field--is-displayed");
     } else {
       newDisplayed =
         allFields[
         (displayedFieldIndex - 1 + allFields.length) % allFields.length
         ];
       newDisplayedFieldIndex = parseInt(displayedFieldIndex - 1, 10);
-      displayed.classList.add('c-test__field--is-slided-out-right');
-      newDisplayed.classList.add('c-test__field--is-slided-in-left', 'c-test__field--is-displayed');
+      displayed.classList.add("c-test__field--is-slided-out-right");
+      newDisplayed.classList.add("c-test__field--is-slided-in-left", "c-test__field--is-displayed");
     }
 
     switch (newDisplayedFieldIndex) {
@@ -57,22 +57,26 @@ function changeField(forward) {
 }
 
 allFields.forEach(field => {
-  field.addEventListener('transitionend', e => {
+  field.addEventListener("transitionend", e => {
     // Check for the old displayed transition and if clickable is false
     // do not trigger it more than once
     if (field === displayed && !clickable) {
       clickable = true;
       // Remove all CSS animation classes on old displayed
-      displayed.className = 'c-test__field';
+      const displayedFieldIndex = allFields.indexOf(displayed);
+      console.log(displayedFieldIndex);
+      displayed.className = displayedFieldIndex === 10
+        ? "c-test__field c-test__result"
+        : "c-test__field";
     }
   });
 });
 
 //Event listeners
-forwardButton.addEventListener('click', () => {
+forwardButton.addEventListener("click", () => {
   changeField(true);
 });
-backButton.addEventListener('click', () => {
+backButton.addEventListener("click", () => {
   changeField(false);
 });
 
