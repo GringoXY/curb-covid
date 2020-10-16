09f65736de162142fc77b1e25f593592b929afc8
GringoXY <przemyslawkowalski01pk@gmail.com>
2020-10-16 17:09:38 +0200

    Add import

--- src/js/health-test.js
+++ src/js/health-test.js
@@ -3,3 +3,4 @@ import '../scss/contrast.scss';
 import './functionality/font-size-changer';
 import './functionality/contrast-mode';
 import './functionality/hamburger';
+import './functionality/slider';
