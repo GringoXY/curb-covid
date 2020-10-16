3b216b2f9f4e85fd882a05e9af4edd0bd4bfc6d9
GringoXY <przemyslawkowalski01pk@gmail.com>
2020-10-16 10:04:55 +0200

    Add missing import

--- src/js/health-test.js
+++ src/js/health-test.js
@@ -1,4 +1,5 @@
 import '../scss/health-test.scss';
+import '../scss/contrast.scss';
 import './functionality/font-size-changer';
 import './functionality/contrast-mode';
 import './functionality/hamburger';
