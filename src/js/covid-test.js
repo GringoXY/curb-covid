14e42b5e69de345139ef91fa6de028d48235f0b9
Przemysław Sipa <przemeksipa14@gmail.com>
2020-10-17 17:22:20 +0200

    Remove margin bottom

---
+++ src/js/covid-test.js
@@ -0,0 +1,7 @@
+import '../scss/covid-test.scss';
+import '../scss/contrast.scss';
+import './functionality/font-size-changer';
+import './functionality/contrast-mode';
+import './functionality/hamburger';
+import './functionality/slider';
+import './functionality/range-slider';
