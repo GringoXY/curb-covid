7d266f0944e0d02c3be9ac335c97a1f4227d74f3
GringoXY <przemyslawkowalski01pk@gmail.com>
2020-10-18 11:08:48 +0200

    Change result text & add symptoms list generator

--- src/js/functionality/test-result-calculator.js
+++ src/js/functionality/test-result-calculator.js
@@ -1,3 +1,19 @@
+function displayCovidSymptoms(symptoms) {
+  // make covid symptoms as list
+  const listOfSymptoms = document.createElement("ul");
+  listOfSymptoms.className = "c-test__list";
+
+  // create list item on every element
+  symptoms.forEach(symptom => {
+    const listItem = document.createElement("li");
+    listItem.textContent = symptom;
+    listItem.className = "c-test__symptom";
+    listOfSymptoms.appendChild(listItem);
+  });
+
+  return listOfSymptoms;
+}
+
 // catch elements to put in text content depending on result
 const resultTitle = document.querySelector(".c-test__judgement");
 const resultDescription = document.querySelector(".c-test__details");
@@ -16,36 +32,56 @@ btnNext.addEventListener("click", () => {
   const fields = [...document.querySelectorAll(".c-test__field")];
   const displayedField = document.querySelector(".c-test__field--is-displayed");
   displayedFieldIndex = fields.indexOf(displayedField);
+  const covidSymptoms = new Array();
 
   //summarize probability of having disease
   if (displayedFieldIndex === 9) {
     allInputs.forEach(input => {
-      if (input.checked) {
+      const isChecked = input.checked;
+      if (isChecked) {
         const dataProbability = parseInt(input.getAttribute("data-probability"), 10);
         probabilityOfDisease += dataProbability;
+
+        const isCovidSymptom = input.getAttribute("data-text");
+        if (isCovidSymptom) {
+          const symptom = isCovidSymptom;
+          covidSymptoms.push(symptom);
+        }
       }
     });
 
     // depending on probability of having disease display proper result field
     if (probabilityOfDisease > 15) {
-      resultField.className = "c-test__field c-test__field--is-displayed c-test__result c-test__result--red";
-      resultTitle.textContent = "Very bad";
-      resultDescription.textContent = "Call an ambulance immediately!";
+      resultField.className = "c-test__field c-test__field--is-displayed c-test__result c-test__result--orange";
+      resultTitle.textContent = "Call the emergency number. Avoid all contact.";
+
+      // get list of symptoms
+      const symptomsDescription = displayCovidSymptoms(covidSymptoms);
+
+      // set description text
+      resultDescription.textContent = "Your symptoms are very serious and you may have COVID-19.";
+      resultDescription.appendChild(symptomsDescription);
     }
     else if (probabilityOfDisease > 10) {
       resultField.className = "c-test__field c-test__field--is-displayed c-test__result c-test__result--orange";
-      resultTitle.textContent = "Bad";
-      resultDescription.textContent = "Call a doctor";
+      resultTitle.textContent = "Call a doctor";
+
+      // get list of symptoms
+      const symptomsDescription = displayCovidSymptoms(covidSymptoms);
+
+      // set description text
+      resultDescription.textContent = "Your answers do not suggest that you have the COVID-19. However, your symptoms are worrisome and require medical consultation.";
+      resultDescription.appendChild(symptomsDescription);
     }
     else if (probabilityOfDisease > 5) {
       resultField.className = "c-test__field c-test__field--is-displayed c-test__result c-test__result--white";
-      resultTitle.textContent = "You have light symptoms";
-      resultDescription.textContent = "Eat healthy take vitamins & get hot tea with honey and lemon";
+      resultTitle.textContent = "Stay home and monitor your symptoms.";
+      resultDescription.textContent = "Your symptoms do not suggest that you have the COVID-19. Still, it is better if you recover from your illness or symptoms at home for your and other people’s safety. Remember that your symptoms may also result from other diseases and may require medical consultation - this interview targets the COVID-19 infection. If your symptoms seem severe and you are worried, contact your doctor or local health authorities.";
     }
     else {
       resultField.className = "c-test__field c-test__field--is-displayed c-test__result c-test__result--green";
-      resultTitle.textContent = "It's all fine";
-      resultDescription.textContent = "You don't have COVID-19 symptoms. Stay care <3";
+      resultTitle.textContent = "Follow preventive measures.";
+      resultDescription.textContent = "Your symptoms do not suggest that you have COVID-19. Continue following the common measures and government directives to avoid contracting COVID-19. Remember that your symptoms may also result from other diseases and may require medical consultation - this interview targets the COVID-19 infection. If your symptoms seem severe and you are worried, contact your doctor or local health authorities.";
     }
 
   }
