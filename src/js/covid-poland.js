05512ba7454eed5b28c12a0e49e5b9c748abd363
GringoXY <przemyslawkowalski01pk@gmail.com>
2020-10-19 19:04:21 +0200

    Remove imports

--- src/js/covid-poland.js
+++ src/js/covid-poland.js
@@ -2,6 +2,4 @@ import '../scss/covid-poland.scss';
 import '../scss/contrast.scss';
 import './functionality/chart.bundle';
 import './functionality/api-covid-poland';
-import './functionality/font-size-changer';
-import './functionality/contrast-mode';
 import './functionality/hamburger';
