4b97d2ec659d4208e2cd7b015726b61f3255487d
GringoXY <przemyslawkowalski01pk@gmail.com>
2020-10-19 16:12:46 +0200

    Add imports

--- src/js/covid-poland.js
+++ src/js/covid-poland.js
@@ -2,3 +2,6 @@ import '../scss/covid-poland.scss';
 import '../scss/contrast.scss';
 import './functionality/chart.bundle';
 import './functionality/api-covid-poland';
+import './functionality/font-size-changer';
+import './functionality/contrast-mode';
+import './functionality/hamburger';
