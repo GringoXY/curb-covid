e04d49acdb574448bf05d3ffefa80fe64197bf4f
GringoXY <przemyslawkowalski01pk@gmail.com>
2020-10-21 15:56:53 +0200

    Add import

--- src/js/about-us.js
+++ src/js/about-us.js
@@ -4,3 +4,4 @@ import './functionality/hamburger';
 import './functionality/font-size-changer';
 import './functionality/contrast-mode';
 import './functionality/particles';
+import './functionality/slide-in-animation';
