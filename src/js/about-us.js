ade67003d0542c77582e8b28b36fb02c982df50f
GringoXY <przemyslawkowalski01pk@gmail.com>
2020-10-21 09:22:16 +0200

    Add imports

--- src/js/about-us.js
+++ src/js/about-us.js
@@ -1,3 +1,6 @@
 import '../scss/about-us.scss';
+import '../scss/contrast.scss';
 import './functionality/hamburger';
+import './functionality/font-size-changer';
+import './functionality/contrast-mode';
 import './functionality/particles';
